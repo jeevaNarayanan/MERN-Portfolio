251308d4cc94151b85e937b9664604a1c38db32c
Jeeva <jeeva.narayanan2012@gmail.com>
2025-03-08 13:03:34 +0530

    last update

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -1,6 +1,34 @@
+const nodemailer = require('nodemailer')
+const sendGridTransport = require('nodemailer-sendgrid-transport')
+
+//transport
+const transporter = nodemailer.createTransport(
+    sendGridTransport({
+        auth : {
+            api_Key: process.env.API_SENDGRID,
+        },
+    })
+)
+
 const sendEmailController = (req,res) => {
  try {
 
+    const {name,email,msg} = req.body
+
+    if(!name || !email || !mag){
+        return res.status.(500).send({
+            success:false,
+            message: 'Please Provide All Fields'
+        })
+    }
+
+    //email matter
+    transporter.sendMail({
+        from:"jeeva.narayanan2012@gmail.com",
+        subject: 'Regarding MERN P[Portfolio App',
+        
+
+    })
     return res.status(200).send({
         success: true,
         message: "Your Message Send Succesfully",
